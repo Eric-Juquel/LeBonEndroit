ca231b1cfdc17f73bf2836042b177734a7c8bc48
Eric J <ejuquel@yahoo.fr>
2020-04-11 19:46:28 +0200

    step 9

--- wp-content/plugins/wp-Livre-d-or-BE/wp-Livre-d-or.php
+++ wp-content/plugins/wp-Livre-d-or-BE/wp-Livre-d-or.php
@@ -32,63 +32,62 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 Copyright 2005-2015 Automattic, Inc.
 */
 
-// if (! defined( 'ABSPATH')) {
-//   die;
-// };
+
 
 if (!function_exists('add_action')) {
   echo 'Hey, you can\'t access this file !';
   exit;
 }
 
-class LivreDOrPlugin
-{
-  function __construct()
+if (!class_exists('AlecadddPlugin')) {
+  class LivreDOrPlugin
   {
-    add_action('init', array($this, 'custom_post_type'));
+    function __construct()
+    {
+      $this->create_post_type();
+    }
+    protected function create_post_type()
+    {
+      add_action('init', array($this, 'custom_post_type'));
+    }
+
+    function activate()
+    {
+      $this->custom_post_type();
+      flush_rewrite_rules();
+    }
+
+    function deactivate()
+    {
+      flush_rewrite_rules();
+    }
+
+    function register()
+    {
+      add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+    }
+
+    function custom_post_type()
+    {
+      register_post_type('livredor', ['public' => true, 'label' => 'Livre d\'Or']);
+    }
+
+    function enqueue()
+    {
+      // enqueue all our scripts
+      wp_enqueue_style('mypluginstyle', plugins_url('./assets/mystyle.css', __FILE__));
+      wp_enqueue_script('mypluginscript', plugins_url('./assets/myscript.js', __FILE__));
+    }
   }
 
-  function activate()
-  {
-    $this->custom_post_type();
-    flush_rewrite_rules();
-  }
 
-  function deactivate()
-  {
-    flush_rewrite_rules();
-  }
-
-  function register()
-  {
-    add_action('admin_enqueue_scripts', array($this, 'enqueue'));
-  }
-
-  //   function create_post_type()
-  //   {
-  //     add_action('init', array($this, 'custom_post_type'));
-  //   }
-
-  function custom_post_type()
-  {
-    register_post_type('livredor', ['public' => true, 'label' => 'Livre d\'Or']);
-  }
-
-  function enqueue()
-  {
-    // enqueue all our scripts
-    wp_enqueue_style('mypluginstyle', plugins_url('./assets/mystyle.css', __FILE__));
-    wp_enqueue_script('mypluginscript', plugins_url('./assets/myscript.js', __FILE__));
-  }
-}
-
-if (class_exists('LivreDOrPlugin')) {
   $livreDOrPlugin = new LivreDOrPlugin();
   $livreDOrPlugin->register();
+
+
+  // activation
+  register_activation_hook(__FILE__, array($livreDOrPlugin, 'activate'));
+
+  // deactivation
+  register_deactivation_hook(__FILE__, array($livreDOrPlugin, 'deactivate'));
 }
-
-// activation
-register_activation_hook(__FILE__, array($livreDOrPlugin, 'activate'));
-
-// deactivation
-register_deactivation_hook(__FILE__, array($livreDOrPlugin, 'deactivate'));
