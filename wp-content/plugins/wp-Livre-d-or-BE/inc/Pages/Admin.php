9b3c4f0fad1dbdf37d8b7fa5df44dc417bf3a96a
Eric J <ejuquel@yahoo.fr>
2020-04-12 19:45:25 +0200

    dashboard ready

--- wp-content/plugins/wp-Livre-d-or-BE/inc/Pages/Admin.php
+++ wp-content/plugins/wp-Livre-d-or-BE/inc/Pages/Admin.php
@@ -7,32 +7,63 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
+  public $settings;
+  public $pages = array();
+  public $subpages = array();
 
+  public function __construct()
+  {
+    $this->settings = new SettingsApi;
+
+    $this->pages = array(
+      [
+        'page_title' => 'Livred\'Or Plugin',
+        'menu_title' => 'Livre d\'Or',
+        'capability' => 'manage_options',
+        'menu_slug' => 'livredor_plugin',
+        'callback' => function () {
+          echo '<h1>Welcome</h1>';
+        },
+        'icon_url' => 'dashicons-book-alt',
+        'position' => 110
+      ]
+    );
+
+    $this->subpages = array(
+			[
+				'parent_slug' => 'livredor_plugin', 
+				'page_title' => 'Custom Post Types', 
+				'menu_title' => 'CPT', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'livredor_cpt', 
+				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+      ],
+			[
+				'parent_slug' => 'livredor_plugin', 
+				'page_title' => 'Custom Taxonomies', 
+				'menu_title' => 'Taxonomies', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'livredor_taxonomies', 
+				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+      ],
+			[
+				'parent_slug' => 'livredor_plugin', 
+				'page_title' => 'Custom Widgets', 
+				'menu_title' => 'Widgets', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'livredor_widgets', 
+				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+      ]
+		);
+  }
 
   public function register()
   {
 
-    add_action('admin_menu', array($this, 'add_admin_pages'));
-  }
-
-  public function add_admin_pages()
-  {
-    add_menu_page(
-      'Livred\'Or Plugin',
-      'Livre d\'Or',
-      'manage_options',
-      'livredor_plugin',
-      array($this, 'admin_index'),
-      'dashicons-book-alt',
-      110
-    );
-  }
-
-  public function admin_index()
-  {
-    require_once $this->plugin_path . 'templates/admin.php';
+    $this->settings->addPages($this->pages)->withSubPage( 'Dashboard' )->addSubPages( $this->subpages)->register();
   }
 }
